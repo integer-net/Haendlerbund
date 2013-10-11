20c280142346264c6477060a8073859506edf813
zsoerenm <sz@integer-net.de>
2013-10-11 11:28:42 +0200

    added mode option, added default values, bug fixes

--- app/code/community/IntegerNet/Haendlerbund/Model/InsertLegislativeTexts.php
+++ app/code/community/IntegerNet/Haendlerbund/Model/InsertLegislativeTexts.php
@@ -11,33 +11,39 @@ class IntegerNet_Haendlerbund_Model_InsertLegislativeTexts extends Mage_Core_Mod
 {
 	public function run()
     {
-        $apiKey = Mage::getStoreConfig('integernet_haendlerbund/rechtstexte/api_key');
-        $accessToken = Mage::getStoreConfig('integernet_haendlerbund/rechtstexte/access_token');
-        $textKeys = array(
-            'allgemeine_geschaeftsbedingungen'      => '12766C46A8A',
-            'rueckgaberecht_fuer_verbraucher'       => '12766C4A6BE',
-            'widerrufsbelehrung'                    => '12766C53647',
-            'bedingungen_fuer_zahlung_und_versand'  => '12766C58F26',
-            'datenschutzerklaerung'                 => '12766C5E204',
-            'impressum'                             => '1293C20B491',
-            'batteriehinweise'                      => '134CBB4D101',
-        );
-        foreach($textKeys as $identifier => $textKey) {
-            $id = $textKeys[Mage::getStoreConfig('integernet_haendlerbund/rechtstexte/'.$identifier)];
-            if($id != 0) {
-                $uri = 'https://www.hbintern.de/www/hbm/api/live_rechtstexte.htm?APIkey='.$apiKey.'&did='.$textKey.'&AccessToken='.$accessToken;
-                $client = new Zend_Http_Client($uri);
-                $response = $client->request();
-                if(stristr($id,'page')) {
-                    $id = str_replace('page_','',$id);
-                    $model = Mage::getModel('cms/page')->load($id);
+        if(Mage::getStoreConfig('integernet_haendlerbund/rechtstexte/enable_automatic_update')) {
+            $apiKey = Mage::getStoreConfig('integernet_haendlerbund/rechtstexte/api_key');
+            $accessToken = Mage::getStoreConfig('integernet_haendlerbund/rechtstexte/access_token');
+            $mode = Mage::getStoreConfig('integernet_haendlerbund/rechtstexte/mode');
+            $textKeys = array(
+                'allgemeine_geschaeftsbedingungen'      => '12766C46A8A',
+                'rueckgaberecht_fuer_verbraucher'       => '12766C4A6BE',
+                'widerrufsbelehrung'                    => '12766C53647',
+                'bedingungen_fuer_zahlung_und_versand'  => '12766C58F26',
+                'datenschutzerklaerung'                 => '12766C5E204',
+                'impressum'                             => '1293C20B491',
+                'batteriehinweise'                      => '134CBB4D101',
+            );
+            foreach($textKeys as $identifier => $textKey) {
+                $id = Mage::getStoreConfig('integernet_haendlerbund/rechtstexte/'.$identifier);
+                if($id) {
+                    $uri = sprintf(Mage::getStoreConfig('integernet_haendlerbund/rechtstexte/apiuri'), $apiKey, $textKey, $accessToken, $mode);
+
+                    $client = new Zend_Http_Client($uri);
+                    $response = $client->request();
+                    $legistlativeText = $response->getBody();
+
+                    if(stristr($id,'page_')) {
+                        $id = str_replace('page_','',$id);
+                        $model = Mage::getModel('cms/page')->load($id);
+                    }
+                    else {
+                        $id = str_replace('block_','',$id);
+                        $model = Mage::getModel('cms/page')->load($id);
+                    }
+                    $model->setContent($legistlativeText);
+                    $model->save();
                 }
-                else {
-                    $id = str_replace('block_','',$id);
-                    $model = Mage::getModel('cms/page')->load($id);
-                }
-                $model->setContent($response);
-                $model->save();
             }
         }
     }
